9efe8417c813cb0686428dfa87f1e3ecea19b3b6
47546832 <47546832+tunamagur0@users.noreply.github.com>
2022-12-26 07:42:20 +0000

    feat(question): add #21104 - FindAll

---
+++ questions/21104-medium-findall/test-cases.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<FindAll<'Collection of TypeScript type challenges', 'Type'>, [14]>>,
+  Expect<Equal<FindAll<'Collection of TypeScript type challenges', 'pe'>, [16, 27]>>,
+  Expect<Equal<FindAll<'Collection of TypeScript type challenges', ''>, []>>,
+  Expect<Equal<FindAll<'', 'Type'>, []>>,
+  Expect<Equal<FindAll<'', ''>, []>>
+];
