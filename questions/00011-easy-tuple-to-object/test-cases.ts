590b2fb6e3a525fae652188b4ed02fc50910f784
yangxin <yangxin@totodi.com>
2023-02-02 17:31:15 +0800

    feat：tuple to object

--- questions/00011-easy-tuple-to-object/test-cases.ts
+++ questions/00011-easy-tuple-to-object/test-cases.ts
@@ -10,5 +10,7 @@ type cases = [
   Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>,
 ]
 
+type test = TupleToObject<typeof tuple>
+
 // @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>
